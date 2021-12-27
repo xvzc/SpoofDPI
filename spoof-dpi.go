d94bf165c3903dcabf95923b037390a3f25fb27e
jerry901 <pkhu2000@gmail.com>
2021-12-27 17:29:17 +0900

    update

--- spoof-dpi.go
+++ spoof-dpi.go
@@ -1,28 +1,72 @@
 package main
 
 import (
-    "net"
-    "log"
-    "SpoofDPI/handler"
+	"SpoofDPI/mitm"
+	"SpoofDPI/util"
+    "fmt"
+	"log"
+	"net"
 )
+
+const (
+    CLI_PORT = "8080"
+    DNS_ADDR = "1.1.1.1"
+)
+
+type config struct {
+    port string 
+    age  int
+}
 
 func main() {
 	log.Println("##### Listening 8080..")
 
-    listener, err := net.Listen("tcp", ":8080")
+    listener, err := net.Listen("tcp", ":" + CLI_PORT)
 	if err != nil {
 		panic(err)
 	}
+
 	for {
-		connClient, err := listener.Accept()
+		clientConn, err := listener.Accept()
 		if err != nil {
 			log.Println("error accepting connection", err)
 			continue
 		}
 
-		log.Println("##### New connection", connClient.RemoteAddr())
+		log.Println("##### New connection", clientConn.RemoteAddr())
 
-        go handler.HandleClientRequest(connClient)
+        go func() {
+            defer clientConn.Close()
+
+            buf, err := util.ReadBytes(clientConn)
+            if err != nil {
+                return
+            }
+
+            fmt.Println()
+            log.Println()
+            fmt.Println("##### Request from client : ")
+            fmt.Println(string(buf))
+
+            domain, port := util.ExtractDomainAndPort(string(buf))
+
+            log.Println("domain: "+ domain)
+            log.Println("port: " + port)
+
+            ip, err := util.DnsLookupOverHttps(DNS_ADDR, domain) // Dns lookup over https
+            if err != nil {
+                log.Fatal(err)
+                return
+            }
+
+            remoteConn, err := net.Dial("tcp", ip+":"+port) // create connection to server
+            if err != nil {
+                log.Fatal(err)
+                return
+            }
+            defer remoteConn.Close()
+
+            mitm.GoGoSing(clientConn, remoteConn, buf)
+        }()
 	}
 }
-
