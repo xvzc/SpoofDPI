43017e721176574acc1adf1a0bb40671a0835792
xvzc <pkhu2000@gmail.com>
2022-01-05 01:47:18 +0900

    update refactor

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -5,54 +5,54 @@ import (
 	"net"
 	"os"
 
-	"github.com/xvzc/SpoofDPI/util"
 	"github.com/xvzc/SpoofDPI/config"
+	"github.com/xvzc/SpoofDPI/util"
 )
 
 func Start() {
-    listener, err := net.Listen("tcp", ":" + config.GetConfig().Port)
+	listener, err := net.Listen("tcp", ":"+config.GetConfig().Port)
 	if err != nil {
-        log.Fatal("Error creating listener: ", err)
-        os.Exit(1)
+		log.Fatal("Error creating listener: ", err)
+		os.Exit(1)
 	}
 
-    util.Debug("Created a listener")
+	util.Debug("Created a listener")
 
-    for {
+	for {
 		clientConn, err := listener.Accept()
 		if err != nil {
-            log.Fatal("Error accepting connection: ", err)
+			log.Fatal("Error accepting connection: ", err)
 			continue
 		}
 
-        util.Debug("Accepted a new connection.", clientConn.RemoteAddr())
+		util.Debug("Accepted a new connection.", clientConn.RemoteAddr())
 
-        go func() {
-            defer clientConn.Close()
+		go func() {
+			defer clientConn.Close()
 
-            message , err := ReadBytes(clientConn)
-            if err != nil {
-                return
-            }
+			message, err := ReadBytes(clientConn)
+			if err != nil {
+				return
+			}
 
-            util.Debug("Client sent data: ", len(message))
+			util.Debug("Client sent data: ", len(message))
 
-            domain := util.ExtractDomain(&message)
+			domain := util.ExtractDomain(&message)
 
-            ip, err := util.DnsLookupOverHttps(config.GetConfig().DNS, domain) // Dns lookup over https
-            if err != nil {
-                return
-            }
+			ip, err := util.DnsLookupOverHttps(config.GetConfig().DNS, domain) // Dns lookup over https
+			if err != nil {
+				return
+			}
 
-            util.Debug("ip: "+ ip)
+			util.Debug("ip: " + ip)
 
-            if util.ExtractMethod(&message) == "CONNECT" {
-                util.Debug("HTTPS Requested")
-                HandleHttps(clientConn, ip)
-            }else {
-                util.Debug("HTTP Requested.")
-                HandleHttp(clientConn, ip, message)
-            }
-        }()
-    }
+			if util.ExtractMethod(&message) == "CONNECT" {
+				util.Debug("HTTPS Requested")
+				HandleHttps(clientConn, ip)
+			} else {
+				util.Debug("HTTP Requested.")
+				HandleHttp(clientConn, ip, message)
+			}
+		}()
+	}
 }
