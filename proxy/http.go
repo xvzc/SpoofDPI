38db30765b54026af22ace3064eb0e6596845383
xvzc <pkhu2000@gmail.com>
2022-01-08 02:03:46 +0900

    refactor

--- proxy/http.go
+++ proxy/http.go
@@ -7,20 +7,20 @@ import (
 	"github.com/xvzc/SpoofDPI/util"
 )
 
-func HandleHttp(clientConn net.Conn, ip string, message []byte)  {
-    remoteConn, err := net.Dial("tcp", ip+":80") // create connection to server
-    if err != nil {
-        util.Debug(err)
-        return
-    }
-    defer remoteConn.Close()
+func HandleHttp(clientConn net.Conn, ip string, message []byte) {
+	remoteConn, err := net.Dial("tcp", ip+":80") // create connection to server
+	if err != nil {
+		util.Debug(err)
+		return
+	}
+	defer remoteConn.Close()
 
-    util.Debug("[HTTP] Connected to the server.")
+	util.Debug("[HTTP] Connected to the server.")
 
-    go Serve(remoteConn, clientConn, "HTTP")
+	go Serve(remoteConn, clientConn, "HTTP")
 
-    util.Debug("[HTTP] Sending request to the server")
-    fmt.Fprintf(remoteConn, string(message))
+	util.Debug("[HTTP] Sending request to the server")
+	fmt.Fprintf(remoteConn, string(message))
 
-    Serve(clientConn, remoteConn, "HTTP")
+	Serve(clientConn, remoteConn, "HTTP")
 }
