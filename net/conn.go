6f8c435034e6fc13beb1a0b407f9ebe598fb7ecd
xvzc <pkhu2000@gmail.com>
2022-01-11 02:11:30 +0900

    add net module and fixed the other modules

--- net/conn.go
+++ net/conn.go
@@ -1,13 +1,42 @@
-package proxy
+package net
 
 import (
 	"net"
-	// "github.com/xvzc/SpoofDPI/util"
 )
 
 const BUF_SIZE = 1024
 
-func ReadBytes(conn net.Conn) ([]byte, error) {
+type Conn struct {
+	Conn net.Conn
+}
+
+func (conn *Conn) Close() {
+	conn.Conn.Close()
+}
+
+func (conn *Conn) Read(b []byte) (n int, err error) {
+	return conn.Conn.Read(b)
+}
+
+func (conn *Conn) Write(b []byte) (n int, err error) {
+	return conn.Conn.Write(b)
+}
+
+func (conn *Conn) WriteChunks(c [][]byte) (n int, err error) {
+	total := 0
+	for i := 0; i < len(c); i++ {
+		b, err := conn.Write(c[i])
+		if err != nil {
+			return 0, nil
+		}
+
+		b += total
+	}
+
+	return total, nil
+}
+
+func (conn *Conn) ReadBytes() ([]byte, error) {
 	ret := make([]byte, 0)
 	buf := make([]byte, BUF_SIZE)
 
@@ -26,9 +55,9 @@ func ReadBytes(conn net.Conn) ([]byte, error) {
 	return ret, nil
 }
 
-func Serve(from net.Conn, to net.Conn, proto string) {
+func (from *Conn) Serve(to Conn, proto string) {
 	for {
-		buf, err := ReadBytes(from)
+		buf, err := from.ReadBytes()
 		if err != nil {
 			// util.Debug("["+proto+"]"+"Error reading from ", from.RemoteAddr())
 			// util.Debug(err, " Closing the connection.. ")
--- proxy/http.go
+++ proxy/http.go
@@ -1,27 +1,3 @@
 package proxy
 
-import (
-	"fmt"
-	"net"
-
-	"github.com/xvzc/SpoofDPI/packet"
-	// "github.com/xvzc/SpoofDPI/util"
-)
-
-func HandleHttp(clientConn net.Conn, ip string, p *packet.HttpPacket) {
-	remoteConn, err := net.Dial("tcp", ip+":80") // create connection to server
-	if err != nil {
-		// util.Debug(err)
-		return
-	}
-	defer remoteConn.Close()
-
-	// util.Debug("[HTTP] Connected to the server.")
-
-	go Serve(remoteConn, clientConn, "HTTP")
-
-	// util.Debug("[HTTP] Sending request to the server")
-	fmt.Fprintf(remoteConn, string(*p.Raw))
-
-	Serve(clientConn, remoteConn, "HTTP")
-}
+// "github.com/xvzc/SpoofDPI/util"
