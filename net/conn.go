1448a66a18f5b560c04d7ba6ec0d650fb0fd7f86
xvzc <pkhu2000@gmail.com>
2022-01-12 01:59:55 +0900

    update handlers to belong to conn module

--- net/conn.go
+++ net/conn.go
@@ -4,6 +4,8 @@ import (
 	"net"
 
 	log "github.com/sirupsen/logrus"
+	"github.com/xvzc/SpoofDPI/doh"
+	"github.com/xvzc/SpoofDPI/packet"
 )
 
 const BUF_SIZE = 1024
@@ -65,7 +67,82 @@ func (conn *Conn) ReadBytes() ([]byte, error) {
 	return ret, nil
 }
 
-func (from *Conn) Serve(to Conn, proto string) {
+func (lConn *Conn) HandleHttp(p packet.HttpPacket) {
+	ip, err := doh.Lookup(p.Domain)
+	if err != nil {
+		log.Debug("[HTTPS] Error looking up for domain: ", err)
+	}
+	log.Debug("[HTTPS] Found ip over HTTPS: ", ip)
+
+	// Create connection to server
+	rConn, err := Dial("tcp", ip+":80")
+	if err != nil {
+		log.Debug(err)
+		return
+	}
+	defer rConn.Close()
+
+	log.Debug("[HTTP] Connected to the server.")
+
+	go rConn.Serve(lConn, "HTTP")
+
+	_, err = rConn.Write([]byte("HTTP/1.1 200 Connection Established\r\n\r\n"))
+	if err != nil {
+		log.Debug("[HTTP] Error sending request to the server: ", err)
+	}
+	log.Debug("[HTTP] Sent a request to the server")
+
+	go lConn.Serve(&rConn, "HTTP")
+}
+
+func (lConn *Conn) HandleHttps(p packet.HttpPacket) {
+	ip, err := doh.Lookup(p.Domain)
+	if err != nil {
+		log.Debug("[HTTPS] Error looking up for domain: ", err)
+	}
+	log.Debug("[HTTPS] Found ip over HTTPS: ", ip)
+
+	// Create a connection to the requested server
+	rConn, err := Dial("tcp", ip+":443")
+	if err != nil {
+		log.Debug(err)
+		return
+	}
+	defer rConn.Close()
+
+	log.Debug("[HTTPS] Connected to the server.")
+
+	_, err = lConn.Write([]byte("HTTP/1.1 200 Connection Established\r\n\r\n"))
+	if err != nil {
+		log.Debug("[HTTPS] Error sending client hello: ", err)
+	}
+	log.Debug("[HTTPS] Sent 200 Connection Estabalished")
+
+	// Read client hello
+	clientHello, err := lConn.ReadBytes()
+	if err != nil {
+		log.Debug("[HTTPS] Error reading client hello: ", err)
+		log.Debug("Closing connection: ", lConn.RemoteAddr())
+	}
+
+	log.Debug(lConn.RemoteAddr(), "[HTTPS] Client sent hello: ", len(clientHello), "bytes")
+
+	// Generate a go routine that reads from the server
+	go rConn.Serve(lConn, "HTTPS")
+
+	pkt := packet.NewHttpsPacket(clientHello)
+
+	chunks := pkt.SplitInChunks()
+
+	if _, err := rConn.WriteChunks(chunks); err != nil {
+		return
+	}
+
+	// Read from the client
+	lConn.Serve(&rConn, "HTTPS")
+}
+
+func (from *Conn) Serve(to *Conn, proto string) {
 	for {
 		buf, err := from.ReadBytes()
 		if err != nil {
