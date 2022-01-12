72ed91b01b89fc84c481184013f293de7f39060c
xvzc <pkhu2000@gmail.com>
2022-01-12 17:59:40 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!bin/bash
 
-curl --silent "https://api.github.com/repos/xvzc/SpoofDPI/releases/latest" |
+curl "https://api.github.com/repos/xvzc/SpoofDPI/releases/latest" |
     grep '"tag_name":' |                                                
     sed -E 's/.*"([^"]+)".*/\1/' |
-    xargs -I {} curl -sOL "https://github.com/xvzc/SpoofDPI/releases/download/"\{\}'/spoof-dpi-osx.tar.gz'
+    xargs -I {} curl -OL "https://github.com/xvzc/SpoofDPI/releases/download/"\{\}"/spoof-dpi-${1}.tar.gz"
 
-tar -xzvf ./spoof-dpi-osx.tar.gz && rm -rf ./spoof-dpi-osx.tar.gz
+tar -xzvf ./spoof-dpi-osx.tar.gz && rm -rf ./spoof-dpi-${1}.tar.gz
