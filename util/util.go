fa30ec0f78bfa703f24e42184dc28e2048ec681a
xvzc <pkhu2000@gmail.com>
2022-07-24 11:20:23 +0900

    add banner mode off option

--- util/util.go
+++ util/util.go
@@ -7,18 +7,19 @@ import (
 	"github.com/pterm/pterm"
 )
 
-func ParseArgs() (string, int, string, bool) {
+func ParseArgs() (string, int, string, bool, bool) {
 	addr := flag.String("addr", "127.0.0.1", "Listen addr")
 	port := flag.Int("port", 8080, "port")
 	dns := flag.String("dns", "8.8.8.8", "DNS server")
 	debug := flag.Bool("debug", false, "true | false")
+	banner := flag.Bool("banner", true, "true | false")
 
 	flag.Parse()
 
-	return *addr, *port, *dns, *debug
+	return *addr, *port, *dns, *debug, *banner
 }
 
-func PrintWelcome(addr string, port int, dns string, debug bool) {
+func PrintColoredBanner(addr string, port int, dns string, debug bool) {
 	cyan := pterm.NewLettersFromStringWithStyle("Spoof", pterm.NewStyle(pterm.FgCyan))
 	purple := pterm.NewLettersFromStringWithStyle("DPI", pterm.NewStyle(pterm.FgLightMagenta))
 	pterm.DefaultBigText.WithLetters(cyan, purple).Render()
@@ -29,4 +30,13 @@ func PrintWelcome(addr string, port int, dns string, debug bool) {
 		{Level: 0, Text: "DNS   : " + dns},
 		{Level: 0, Text: "DEBUG : " + fmt.Sprint(debug)},
 	}).Render()
+}
+
+func PrintSimpleInfo(addr string, port int, dns string, debug bool) {
+    fmt.Println("")
+    fmt.Println("- ADDR  : ", addr)
+    fmt.Println("- PORT  : ", port)
+    fmt.Println("- DNS   : ", dns)
+    fmt.Println("- DEBUG : ", debug)
+    fmt.Println("")
 }
