bdc1a68691173a20b791f6bca83905cd0004f8c8
xvzc <dev.kwanghoo@gmail.com>
2024-08-15 01:58:52 +0900

    add windows release

--- make-releases.sh
+++ make-releases.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-for osarch in 'darwin/amd64' 'darwin/arm64' 'linux/amd64' 'linux/arm' 'linux/arm64' 'linux/mips' 'linux/mipsle'; do
+for osarch in 'darwin/amd64' 'darwin/arm64' 'linux/amd64' 'linux/arm' 'linux/arm64' 'linux/mips' 'linux/mipsle' ; do
     GOOS=${osarch%/*} GOARCH=${osarch#*/} go build -ldflags="-w -s" github.com/xvzc/SpoofDPI/cmd/spoof-dpi &&
         tar -zcvf spoof-dpi-${osarch%/*}-${osarch#*/}.tar.gz ./spoof-dpi &&
         rm -rf ./spoof-dpi
+done
+
+for osarch in 'windows/amd64'; do
+    GOOS=${osarch%/*} GOARCH=${osarch#*/} go build -o spoof-dpi-${osarch%/*}-${osarch#*/}.exe -ldflags="-w -s" github.com/xvzc/SpoofDPI/cmd/spoof-dpi 
 done
