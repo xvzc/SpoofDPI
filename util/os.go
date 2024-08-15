c6947eb0b8e48c452ec5affd122f951533f0139e
incubus013 <incubus013@local>
2024-08-15 23:28:12 +0700

    Setting up a system-wide proxy (windows)

--- util/os.go
+++ util/os.go
@@ -1,55 +1,12 @@
+//go:build !windows && !darwin
+// +build !windows,!darwin
+
 package util
 
-import (
-	"fmt"
-	"os/exec"
-	"runtime"
-	"strings"
-)
-
 func SetOsProxy(port int) error {
-	if runtime.GOOS != "darwin" {
-		return nil
-	}
-
-	network, err := exec.Command("sh", "-c", "networksetup -listnetworkserviceorder | grep `route -n get 0.0.0.0 | grep 'interface' | cut -d ':' -f2` -B 1 | head -n 1 | cut -d ' ' -f 2-").Output()
-
-	if err != nil {
-		return err
-	}
-
-	_, err = exec.Command("sh", "-c", "networksetup -setwebproxy "+"'"+strings.TrimSpace(string(network))+"'"+" 127.0.0.1 "+fmt.Sprint(port)).Output()
-	if err != nil {
-		return err
-	}
-
-	_, err = exec.Command("sh", "-c", "networksetup -setsecurewebproxy "+"'"+strings.TrimSpace(string(network))+"'"+" 127.0.0.1 "+fmt.Sprint(port)).Output()
-	if err != nil {
-		return err
-	}
-
 	return nil
 }
 
 func UnsetOsProxy() error {
-	if runtime.GOOS != "darwin" {
-		return nil
-	}
-
-	network, err := exec.Command("sh", "-c", "networksetup -listnetworkserviceorder | grep `route -n get 0.0.0.0 | grep 'interface' | cut -d ':' -f2` -B 1 | head -n 1 | cut -d ' ' -f 2-").Output()
-	if err != nil {
-		return err
-	}
-
-	_, err = exec.Command("sh", "-c", "networksetup -setwebproxystate "+"'"+strings.TrimSpace(string(network))+"'"+" off").Output()
-	if err != nil {
-		return err
-	}
-
-	_, err = exec.Command("sh", "-c", "networksetup -setsecurewebproxystate "+"'"+strings.TrimSpace(string(network))+"'"+" off").Output()
-	if err != nil {
-		return err
-	}
-
 	return nil
 }
