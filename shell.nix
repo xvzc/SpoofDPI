20c68d3d470a0758e1e5484ec988d1bdab95d6d9
xvzc <me@xvzc.dev>
2025-10-29 22:40:07 +0900

    chore: add shell.nix

---
+++ shell.nix
@@ -0,0 +1,21 @@
+{
+  pkgs ? import <nixpkgs> { },
+}:
+let
+  unstableTarball = builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
+
+  unstablePkgs = import unstableTarball { };
+in
+pkgs.mkShell {
+  packages = with pkgs; [
+    go
+    gopls
+    golangci-lint-langserver
+    unstablePkgs.golangci-lint
+  ];
+
+  shellHook = # sh
+    ''
+      export name="nix:SpoofDPI"
+    '';
+}
