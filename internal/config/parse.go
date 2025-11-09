6f3d1fbcdc52da360f8be11e7b5894acecd072de
xvzc <me@xvzc.dev>
2025-11-09 23:12:46 +0900

    feat(config): add support for toml configuration

--- internal/config/parse.go
+++ internal/config/parse.go
@@ -1,15 +1,1 @@
 package config
-
-import (
-	"regexp"
-)
-
-func parseAllowedPatterns(patterns StringArray) []*regexp.Regexp {
-	var allowedPatterns []*regexp.Regexp
-
-	for _, pattern := range patterns {
-		allowedPatterns = append(allowedPatterns, regexp.MustCompile(pattern))
-	}
-
-	return allowedPatterns
-}
