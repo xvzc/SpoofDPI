2484e9043f4119d8d1d76867566d662c3a384c9d
xvzc <me@xvzc.dev>
2025-11-09 23:24:03 +0900

    feat: reorganize main function to use urfave/cli

--- cmd/spoofdpi/main.go
+++ cmd/spoofdpi/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"os/signal"
@@ -19,25 +20,24 @@ import (
 )
 
 func main() {
-	args := config.ParseArgs()
-	if args.Version {
-		version.PrintVersion()
-		os.Exit(0)
+	cmd := config.CreateCommand(runApp)
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func runApp(ctx context.Context, cfg *config.Config) {
+	if !cfg.Silent {
+		printBanner()
 	}
 
-	cfg := config.LoadConfigurationFromArgs(
-		args,
-		applog.WithScope(applog.NewLogger(args.Debug), "CONFIG"),
-	)
+	baseLogger := applog.NewLogger(cfg.Debug)
+	logger := applog.WithScope(baseLogger, "SETUP")
+	logger.Info().Msgf("started spoofdpi %s", version.Version())
 
-	if !cfg.Silent() {
-		printBanner(cfg)
-	}
-
-	baseLogger := applog.NewLogger(cfg.Debug())
-	logger := applog.WithScope(baseLogger, "MAIN")
-
-	p, err := buildApp(cfg, baseLogger, logger)
+	resolver := createResolver(cfg, baseLogger)
+	p, err := createProxy(cfg, resolver, baseLogger, logger)
 	if err != nil {
 		logger.Fatal().Msgf("failed to build app: %s", err)
 	}
@@ -46,15 +46,32 @@ func main() {
 	go p.Start()
 
 	// set system-wide proxy configuration.
-	if cfg.SetSystemProxy() {
-		if err := system.SetProxy(cfg.ListenPort()); err != nil {
+	if cfg.SetSystemProxy {
+		if err := system.SetProxy(cfg.ListenPort.Value(), logger); err != nil {
 			logger.Fatal().Msgf("error while changing proxy settings: %s", err)
 		}
 		defer func() {
-			if err := system.UnsetProxy(); err != nil {
+			if err := system.UnsetProxy(logger); err != nil {
 				logger.Fatal().Msgf("error while disabling proxy: %s", err)
 			}
 		}()
+	} else {
+		logger.Info().Msgf("use '--system-proxy' to automatically set system proxy")
+	}
+
+	if cfg.Timeout.Value() > 0 {
+		logger.Info().
+			Msgf("connection timeout is set to %d ms", cfg.Timeout.Value())
+	}
+
+	logger.Info().Msgf(
+		"patterns: allow %d, ignore %d", len(cfg.PatternsAllowed), len(cfg.PatternsIgnored),
+	)
+
+	logger.Info().Msgf("resolver info;")
+	dnsInfo := resolver.Info()
+	for i := range dnsInfo {
+		logger.Info().Msgf(" • %s", dnsInfo[i].String())
 	}
 
 	sigs := make(chan os.Signal, 1)
@@ -75,42 +92,37 @@ func main() {
 	<-done
 }
 
-func buildApp(
-	cfg *config.Config,
-	baseLogger zerolog.Logger,
-	logger zerolog.Logger,
-) (*proxy.Proxy, error) {
+func createResolver(cfg *config.Config, baseLogger zerolog.Logger) dns.Resolver {
 	// create a local resolver.
 	localResolver := dns.NewLocalResolver(
-		cfg.DnsQueryTypes(),
+		cfg.GenerateDnsQueryTypes(),
 		applog.WithScope(baseLogger, "DNS(LOCAL)"),
 	)
 
 	// create a plain resolver that uses UDP on port 53.
 	plainResolver := dns.NewPlainResolver(
-		cfg.DnsAddr(),
-		cfg.DnsPort(),
-		cfg.DnsQueryTypes(),
+		cfg.DnsAddr.Value(),
+		cfg.DnsPort.Value(),
+		cfg.GenerateDnsQueryTypes(),
 		applog.WithScope(baseLogger, "DNS(PLAIN)"),
 	)
 
 	// create a resolver for DNS over HTTPS (DoH).
 	httpsResolver := dns.NewHTTPSResolver(
-		cfg.DOHEndpoint(),
-		cfg.DnsAddr(),
-		cfg.DnsQueryTypes(),
+		cfg.GenerateDOHEndpoint(),
+		cfg.GenerateDnsQueryTypes(),
 		applog.WithScope(baseLogger, "DNS(HTTPS)"),
 	)
 
 	// create a TTL cache for storing DNS records.
 	dnsCache := datastruct.NewTTLCache[dns.RecordSet](
-		cfg.CacheShards(),
+		cfg.CacheShards.Value(),
 		time.Duration(1*time.Minute),
 	)
 
 	// create a resolver that routes DNS queries based on rules.
-	routeResolver := dns.NewRouteResolver(
-		cfg.EnableDOH(),
+	return dns.NewRouteResolver(
+		cfg.ShouldEnableDOH(),
 		localResolver,
 		dns.NewCacheResolver(
 			dnsCache,
@@ -124,10 +136,17 @@ func buildApp(
 		),
 		applog.WithScope(baseLogger, "DNS(ROUTE)"),
 	)
+}
 
+func createProxy(
+	cfg *config.Config,
+	resolver dns.Resolver,
+	baseLogger zerolog.Logger,
+	logger zerolog.Logger,
+) (*proxy.Proxy, error) {
 	var packetInjector *packet.PacketInjector
 	var hopTracker *packet.HopTracker
-	if cfg.FakeHTTPSPackets() > 0 {
+	if cfg.FakeHTTPSPackets.Value() > 0 {
 		// find the default network interface.
 		iface, err := system.FindDefaultInterface()
 		if err != nil {
@@ -159,8 +178,8 @@ func buildApp(
 		}
 
 		hopCache := datastruct.NewTTLCache[uint8](
-			cfg.CacheShards(),
-			time.Duration(1)*time.Minute,
+			cfg.CacheShards.Value(),
+			time.Duration(3)*time.Minute,
 		)
 		hopTracker = packet.NewHopTracker(handle, hopCache,
 			applog.WithScope(baseLogger, "HOP_TRACK"),
@@ -185,49 +204,43 @@ func buildApp(
 
 	// create an HTTPS handler.
 	httpsHandler := proxy.NewHttpsHandler(
-		cfg.WindowSize(),
-		cfg.FakeHTTPSPackets(),
+		cfg.WindowSize.Value(),
+		cfg.FakeHTTPSPackets.Value(),
 		hopTracker,
 		packetInjector,
 		applog.WithScope(baseLogger, "HTTPS"),
 	)
 
 	return proxy.NewProxy(
-		cfg.ListenAddr(),
-		cfg.ListenPort(),
-		cfg.PatternsAllowed(),
-		cfg.PatternsIgnored(),
-		cfg.Timeout(),
-		routeResolver,
+		cfg.ListenAddr.Value(),
+		cfg.ListenPort.Value(),
+		config.ParseRegexpSlices(cfg.PatternsAllowed),
+		config.ParseRegexpSlices(cfg.PatternsIgnored),
+		time.Duration(cfg.Timeout.Value())*time.Millisecond,
+		resolver,
 		httpHandler,
 		httpsHandler,
 		applog.WithScope(baseLogger, "PROXY"),
 	), nil
 }
 
-func printBanner(cfg *config.Config) {
+func printBanner() {
 	const banner = `
  .d8888b.                              .d888 8888888b.  8888888b. 8888888
-d88P  Y88b                            d88P"  888  "Y88b 888   Y88b  888
+d88P  Y88b                            d88P'  888  'Y88b 888   Y88b  888
 Y88b.                                 888    888    888 888    888  888
- "Y888b.   88888b.   .d88b.   .d88b.  888888 888    888 888   d88P  888
-    "Y88b. 888 "88b d88""88b d88""88b 888    888    888 8888888P"   888
-      "888 888  888 888  888 888  888 888    888    888 888         888
+ 'Y888b.   88888b.   .d88b.   .d88b.  888888 888    888 888   d88P  888
+    'Y88b. 888 '88b d88''88b d88''88b 888    888    888 8888888P'   888
+      '888 888  888 888  888 888  888 888    888    888 888         888
 Y88b  d88P 888 d88P Y88..88P Y88..88P 888    888  .d88P 888         888
- "Y8888P"  88888P"   "Y88P"   "Y88P"  888    8888888P"  888       8888888
+ 'Y8888P'  88888P'   'Y88P'   'Y88P'  888    8888888P'  888       8888888
            888
            888
            888
+
 `
 
 	fmt.Print(banner)
-	fmt.Printf("\n")
-	fmt.Printf(" • LISTEN_ADDR : %s\n", fmt.Sprint(cfg.ListenAddr()))
-	fmt.Printf(" • LISTEN_PORT : %s\n", fmt.Sprint(cfg.ListenPort()))
-	fmt.Printf(" • DNS_ADDR    : %s\n", fmt.Sprint(cfg.DnsAddr()))
-	fmt.Printf(" • DNS_PORT    : %s\n", fmt.Sprint(cfg.DnsPort()))
-	fmt.Printf(" • DEBUG       : %s\n", fmt.Sprint(cfg.Debug()))
-	fmt.Printf("\n")
 	fmt.Printf("Press 'CTRL + c' to quit\n")
 	fmt.Printf("\n")
 }
